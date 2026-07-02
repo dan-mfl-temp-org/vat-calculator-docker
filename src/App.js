7d43034b1a7a981cf29e17a3bd5eff35a643cdad
Dan Kendall <danstuken@gmail.com>
2026-07-02 12:20:02 +0100

    Some stylistic tweaks

--- src/App.js
+++ src/App.js
@@ -35,8 +35,8 @@ function App() {
   };
 
   return (
-    <div className='header field'>
-      VAT CALCULATOR
+    <div className='header main'>
+      <h1>VAT Calculator from Dan Soft</h1>  
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
